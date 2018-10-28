795d82005920ea4220287cf741bd28718a720599
Neal Fesefeldt <neal@Neals-MacBook-Pro.local>
2018-10-27 22:21:57 -0600

    Got AJAX searches working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   resources :enrollments
   resources :students
-  resources :sections
-  resources :courses
+  resources :sections do
+    collection do
+      get 'search'
+    end
+  end
+  resources :courses do
+    collection do
+      get 'search'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
